af575b37ad66d8ac682805285dc926023d88f0cb
Lee Briggs <lbriggs@apptio.com>
2013-11-29 04:47:41 -0800

    Adding support for disabling file archiving

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -20,6 +20,7 @@ class puppet::agent (
   $run_in_noop                  = 'false',
   $cron_command                 = '/usr/bin/puppet agent --onetime --ignorecache --no-daemonize --no-usecacheonfailure --detailed-exitcodes --no-splay',
   $run_at_boot                  = 'true',
+  $archive_files		= 'true',
   $puppet_binary                = '/usr/bin/puppet',
   $symlink_puppet_binary_target = '/usr/local/bin/puppet',
   $symlink_puppet_binary        = 'false',
